ae40c34b4decb6d17016e2bbfca61d07531f0ffb
Sihle Kambula <kambulasihle@gamil.com>
2022-05-07 22:03:45 +0200

    set up react query for data fetching

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import DetailsPage from "./pages/DetailsPage";
 import HomePage from "./pages/HomePage";
+
 export default function App() {
+  // react query client
+  const queryClient = new QueryClient();
   return (
-    <div>
+    <QueryClientProvider client={queryClient}>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path=':imageId' element={<DetailsPage />} />
       </Routes>
-    </div>
+    </QueryClientProvider>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,24 @@
+import { Container } from "@mui/material";
 import React from "react";
+import { useQuery } from "react-query";
 
 export default function HomePage() {
+  const APIkey = "6473511-0417f2cad683f1bee54cafe15";
+  const getImages = async () => {
+    const res = await fetch(`https://pixabay.com/api/?key=${APIkey}&q=wolf`);
+    return res.json();
+  };
+
+  // using react query for async data fetching and caching
+  const { data, isLoading, isError, isSuccess } = useQuery("images", getImages);
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Container>
+      {isLoading && <p>Loading</p>}
+      {isError && <p>Something went wrong</p>}
+      {isSuccess &&
+        data.hits.map((hit) => (
+          <img src={hit.imageURL} alt={hit.id} style={{ width: "200px" }} />
+        ))}
+    </Container>
   );
 }
