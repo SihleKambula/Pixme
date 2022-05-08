ec066c668d99c3a4ddaa76a5a9d846cc84f0bb25
Sihle Kambula <kambulasihle@gamil.com>
2022-05-08 22:14:21 +0200

    added image single image query to detail page

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,20 +1,20 @@
 import React from "react";
-import Masonry from "@mui/lab/Masonry";
-import { Box } from "@mui/system";
 import { ImageList, ImageListItem } from "@mui/material";
-
+import { Link } from "react-router-dom";
 export default function ImageGrid({ data }) {
-  console.log(data);
   return (
     <ImageList variant='masonry' cols={3} gap={8} sx={{ marginTop: "10px" }}>
       {data.hits.map((image) => (
-        <ImageListItem key={image.id}>
-          <img
-            src={`${image.imageURL}`}
-            srcSet={`${image.imageURL}`}
-            loading='lazy'
-          />
-        </ImageListItem>
+        <Link to={`${image.id}`} key={image.id}>
+          <ImageListItem>
+            <img
+              src={`${image.imageURL}`}
+              srcSet={`${image.imageURL}`}
+              alt={`${image.tags}`}
+              loading='lazy'
+            />
+          </ImageListItem>
+        </Link>
       ))}
     </ImageList>
   );
--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,11 +1,51 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { Grid, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 export default function DetailsPage() {
   const params = useParams();
-  console.log(params);
+  const { imageId } = params;
+  //todo:remove API key
+  const APIkey = "6473511-0417f2cad683f1bee54cafe15";
+
+  //todo:create custom hook for this function
+  const getImages = async () => {
+    const res = await fetch(
+      `https://pixabay.com/api/?key=${APIkey}&id=${imageId}`
+    );
+    return res.json();
+  };
+
+  // using react query for async data fetching and caching
+  const { data, isLoading, isError, isSuccess } = useQuery("images", getImages);
+
   return (
-    <div>
-      <h1>{params.imageId}</h1>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={10} md={6}>
+        <img src={`${data.hits[0].imageURL}`} style={{ width: "100%" }} />
+      </Grid>
+      <Grid item xs={10} md={6}>
+        <Typography>
+          Photographer:
+          {data.hits[0].user}
+        </Typography>
+        <Typography>
+          Tags:
+          {data.hits[0].tags}
+        </Typography>
+        <Typography>
+          Views:
+          {data.hits[0].views}
+        </Typography>
+        <Typography>
+          Downloads:
+          {data.hits[0].downloads}
+        </Typography>
+        <Typography>
+          Size:
+          {(data.hits[0].imageSize * 0.000001).toString().slice(0, 3)}MB
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
