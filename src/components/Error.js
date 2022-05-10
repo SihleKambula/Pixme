a4127ccbcba512810dc27c57a012373908a93177
Sihle Kambula <kambulasihle@gamil.com>
2022-05-10 20:55:21 +0200

    added a error message component

---
+++ src/components/Error.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Error() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <p>Something went wrong</p>
+    </div>
+  );
+}
