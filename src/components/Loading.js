eb7f18cf4d5d1a7dff23f4b7d4b6a7135b0fd098
Sihle Kambula <kambulasihle@gamil.com>
2022-05-10 20:54:44 +0200

    centered Loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,5 +2,16 @@ import React from "react";
 import { Triangle } from "react-loader-spinner";
 
 export default function Loading() {
-  return <Triangle height='100' width='100' color='cyan' ariaLabel='loading' />;
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Triangle height='100' width='100' color='cyan' ariaLabel='loading' />
+    </div>
+  );
 }
