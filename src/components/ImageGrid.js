c4ba3fa28c015a80649667c290be68d8c84610fa
Sihle Kambula <kambulasihle@gamil.com>
2022-05-10 23:53:45 +0200

    responsive grid layout

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,12 +1,35 @@
 import React from "react";
-import { ImageList, ImageListItem } from "@mui/material";
+import { Grid, ImageList, ImageListItem, ThemeProvider } from "@mui/material";
 import { Link } from "react-router-dom";
-import { Box } from "@mui/system";
+import { createTheme } from "@mui/system";
 import Image from "./Image";
 export default function ImageGrid({ data }) {
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 300, // phone
+        sm: 600, // tablets
+        md: 900, // small laptop
+      },
+    },
+  });
   return (
-    <Box>
-      <ImageList variant='masonry' cols={3} gap={8} sx={{ marginTop: "10px" }}>
+    <ThemeProvider theme={theme}>
+      <Grid container sx={{ display: { md: "none", xs: "block" } }}>
+        {data.map((image, index) => (
+          <Grid item xs={12} sm={6} key={index}>
+            <Link to={`${image.id}`} data-testid={`img-${index}`}>
+              <Image imageURL={image.imageURL} imageID={image.id} />
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+      <ImageList
+        variant='masonry'
+        cols={3}
+        gap={8}
+        sx={{ marginTop: "10px", display: { xs: "none", md: "block" } }}
+      >
         {data.map((image, index) => (
           <Link to={`${image.id}`} key={index} data-testid={`img-${index}`}>
             <ImageListItem>
@@ -15,6 +38,6 @@ export default function ImageGrid({ data }) {
           </Link>
         ))}
       </ImageList>
-    </Box>
+    </ThemeProvider>
   );
 }
